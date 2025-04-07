1651a09bd743bfb76e70c18033239dc04b289057
Qin Yifan <156170866+Qinyifan-hit@users.noreply.github.com>
2025-04-07 19:50:28 +0800

    Create eval.py

---
+++ MAPPO/eval.py
@@ -0,0 +1,20 @@
+import numpy as np
+def eval_func(env, model, e_turns, agents):
+    num_agent = len(agents)
+    score = np.zeros(num_agent)
+    for _ in range(e_turns):
+        s, _ = env.reset(seed=10)
+        done = False
+        while not done:
+            a_list = [model.action_select(s[agent], True) for agent in agents]
+            a = {agents[a_id]: a_list[a_id] for a_id in range(num_agent)}
+            s_, r, dw, tr, _ = env.step(a)
+            dones = {agent: (dw[agent] or tr[agent]) for agent in agents}
+            done_list = [np.array(dones[agent], dtype=np.bool_) for agent in agents]
+
+            done = any(done_list)
+            for a_id in range(num_agent):
+                score[a_id] += r[agents[a_id]]
+            s = s_
+
+    return score / e_turns
