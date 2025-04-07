0befd82302224fcd3341b2422474974966af1f59
Qin Yifan <156170866+Qinyifan-hit@users.noreply.github.com>
2025-04-07 19:51:08 +0800

    Create Env_Name.py

---
+++ MAPPO/Env_Name.py
@@ -0,0 +1,46 @@
+from pettingzoo.mpe import (
+    simple_adversary_v3,
+    simple_crypto_v3,
+    simple_push_v3,
+    simple_reference_v3,
+    simple_speaker_listener_v4,
+    simple_spread_v3,
+    simple_tag_v3,
+    simple_v3,
+    simple_world_comm_v3,
+)
+
+Name={
+    1: "simple_adversary_v3",
+    2: "simple_crypto_v3",
+    3: "simple_push_v3",
+    4: "simple_reference_v3",
+    5: "simple_speaker_listener_v4",
+    6: "simple_spread_v3",
+    7: "simple_tag_v3",
+    8: "simple_v3",
+    9: "simple_world_comm_v3"
+}
+ENV_MAP = {
+    "simple_adversary_v3": simple_adversary_v3,
+    "simple_crypto_v3": simple_crypto_v3,
+    "simple_push_v3": simple_push_v3,
+    "simple_reference_v3": simple_reference_v3,
+    "simple_speaker_listener_v4": simple_speaker_listener_v4,
+    "simple_spread_v3": simple_spread_v3,
+    "simple_tag_v3": simple_tag_v3,
+    "simple_v3": simple_v3,
+    "simple_world_comm_v3": simple_world_comm_v3,
+}
+
+Brief_Name = {
+    1: "sav3",
+    2: "scv3",
+    3: "spv3",
+    4: "srv3",
+    5: "sslv4",
+    6: "ssv3",
+    7: "stv3",
+    8: "sv3",
+    9: "swcv3"
+}
